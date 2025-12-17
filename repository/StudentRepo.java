efaf52d7833f39520043af1ba97ae5f3c70a1db8
bala-amypo <bala@amypo.in>
2025-12-17 05:21:21 +0000

    User commit

--- repository/StudentRepo.java
+++ repository/StudentRepo.java
@@ -1,3 +1,10 @@
-public interface StudentRepo extends JpaRepository<student,Long>{
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entity.Student;
+
+@Repository
+public interface  StuentRepo extends  JpaRepository<Student,Long> {
     
 }
---
+++ src/main/java/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.example.project.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project.entity.Studententity;
+import com.example.project.service.Studentservice;
+
+@RestController
+public class Studentcontroller {
+    @Autowired
+    Studentservice src;
+    @PostMapping("/post")
+    public Studententity postdata(@RequestBody  Studententity st){
+        return src.savedata(st);
+    }
+    @GetMapping("/get")
+    public List<Studententity>getdata(){
+        return src.retdata();
+    }
+    @GetMapping("/getid/{id}")
+    public Studententity getIdVal(@PathVariable int id){
+        return src.id(id);
+
+    }
+    @PutMapping("/update/{id}")
+    public Studententity putdata(@PathVariable int id,@RequestBody Studententity st){
+        return src.retputdata(id,st);
+    }
+    @DeleteMapping("/delete/{id}")
+    public Studententity deletedata(@PathVariable int id){
+        return src.retdeldata(id);
+    }
+}
