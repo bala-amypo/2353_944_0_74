e762a8168ccf28323a58373fff049a342c0992cb
bala-amypo <bala@amypo.in>
2025-12-18 06:27:27 +0000

    User commit

--- src/main/java/config/SwaggerConfig.java
+++ src/main/java/config/SwaggerConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class Swaggerconfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
